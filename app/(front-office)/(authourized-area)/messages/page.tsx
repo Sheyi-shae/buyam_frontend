4f24568b61d391aecf28c04446dd5f19e32a09ae
Seyi Adeniyi <adeniyiemmanuel042@gmail.com>
2026-05-02 00:35:49 +0100

    error-fix5

--- app/(front-office)/(authourized-area)/messages/page.tsx
+++ app/(front-office)/(authourized-area)/messages/page.tsx
@@ -1,10 +1,17 @@
+import { PageLoader } from "@/components/loading-spinners";
 import MessageLayout from "@/components/message-system/message-layout";
+import { Suspense } from "react";
 
 
 export default function page() {
   return (
-    <div className="pt-16 md:pt-28   max-h-screen overflow-y-auto">
-      <MessageLayout />
-    </div>
+    
+    <Suspense fallback={<PageLoader/>}>
+      <div className="pt-16 md:pt-28   max-h-screen overflow-y-auto">
+        <MessageLayout />
+        </div>
+      </Suspense>
+      
+    
   )
 }
